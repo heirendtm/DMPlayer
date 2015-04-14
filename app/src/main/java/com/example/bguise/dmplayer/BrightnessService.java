3b021f743b565d0927fb5b8d6ebf015297431ace
heirendtm <heirendtm@gmail.com>
2015-04-14 00:48:00 -0400

    BrightnessService -restructure -setup for final project WebService -copied from lab -still needs integration

--- app/src/main/java/com/example/bguise/dmplayer/BrightnessService.java
+++ app/src/main/java/com/example/bguise/dmplayer/BrightnessService.java
@@ -16,8 +16,20 @@ public class BrightnessService extends IntentService {
     public static final String ACTION = "com.example.bguise.dmplayer";
     private final Handler handler = new Handler();
     Intent intent;
-    String bm;
-    double brightness;
+    Bitmap bm;
+
+    int recently_used;
+    int brightness;
+
+    int length;
+    int frequency;
+    String id;
+    String name;
+    int[] brightness_values; //size = length*frequency; brightness_values[timestamp*frequency] = brightness for current timestamp;
+
+    public BrightnessService(){
+        super("BrightnessService");
+    }
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -28,9 +40,79 @@ public class BrightnessService extends IntentService {
         super(name);
     }
 
+
+
     @Override
     protected void onHandleIntent(Intent intent) {
+        if(intent.hasExtra("type")==true){
+            if(intent.getStringExtra("type").compareTo("initialize")==0){
+                length = intent.getIntExtra("length", -1);
+                frequency = intent.getIntExtra("frequency", -1);
+                id = intent.getStringExtra("id");
+                name = intent.getStringExtra("name");
+                brightness_values = new int[length*frequency];
+                for(int i = 0; i < length*frequency; i++){
+                    brightness_values[i] = -1;
+                }
+                startTimer();
+            }
+            else{
+                bm = (Bitmap) intent.getParcelableExtra("bitmap");
+                int time = intent.getIntExtra("time", -1);
+                brightness = calculateBrightness(bm);
+                brightness_values[time*frequency] = brightness;
 
+                recently_used = 1;
+            }
+        }
+    }
+
+    private void startTimer() {
+        handler.postDelayed(watchDog, 60*1000);
+    }
+
+    //function to be run using the handler
+    private Runnable watchDog = new Runnable() {
+        public void run() {
+            checkRecentlyUsed();
+        }
+    };
+
+    //sets up the information to be displayed by putting in an intent
+    private void checkRecentlyUsed() {
+        if(recently_used == 0){
+            exportData();
+            this.stopSelf();
+        }
+        else{
+            recently_used = 0;
+        }
+        handler.postDelayed(watchDog, 60*1000);
+    }
+
+    private void exportData() {
+
+    }
+
+    private int calculateBrightness(Bitmap bitmap) {
+        long red = 0;
+        long green = 0;
+        long blue = 0;
+        long pixelCount = 0;
+        for (int y = bitmap.getHeight()/3; y < 2*bitmap.getHeight()/3; y+=5)
+        {
+            for (int x = 0; x < bitmap.getWidth(); x+=2)
+            {
+                int c = bitmap.getPixel(x, y);
+
+                pixelCount++;
+                red += Color.red(c);
+                green += Color.green(c);
+                blue += Color.blue(c);
+            }
+        }
+        bitmap.recycle();
+        return (int) (100*(0.299*(double) (red/pixelCount / 255.0)+ 0.587*(double) (green / pixelCount / 255.0)+ .114*(double) (blue / pixelCount / 255.0)));
     }
 
     //--------------------------------------------------------------------------
@@ -49,49 +131,7 @@ public class BrightnessService extends IntentService {
         handler.postDelayed(sendUpdatesToUI, 100);
     }
 
-    //function to be run using the handler
-    private Runnable sendUpdatesToUI = new Runnable() {
-        public void run() {
-            DisplayLoggingInfo();
-        }
-    };
 
-    //sets up the information to be displayed by putting in an intent
-    private void DisplayLoggingInfo() {
-        calculateBrightness();
-        intent.putExtra("Brightness", ""+brightness);
-        sendBroadcast(intent);
-        this.stopSelf();
-    }
-
-    private void calculateBrightness() {
-        Bitmap bitmap;
-        try {
-            byte [] encodeByte= Base64.decode(bm, Base64.DEFAULT);
-            bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-        } catch(Exception e) {
-            e.getMessage();
-            return;
-        }
-        long red = 0;
-        long green = 0;
-        long blue = 0;
-        long pixelCount = 0;
-        for (int y = bitmap.getHeight()/3; y < 2*bitmap.getHeight()/3; y+=5)
-        {
-            for (int x = 0; x < bitmap.getWidth(); x+=2)
-            {
-                int c = bitmap.getPixel(x, y);
-
-                pixelCount++;
-                red += Color.red(c);
-                green += Color.green(c);
-                blue += Color.blue(c);
-            }
-        }
-        bitmap.recycle();
-        brightness = 0.299*(double) (red/pixelCount / 255.0)+ 0.587*(double) (green / pixelCount / 255.0)+ .114*(double) (blue / pixelCount / 255.0);
-    }
 
     @Override
     public IBinder onBind(Intent intent) {
